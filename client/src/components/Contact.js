90d54b877aea62b777fa78de095636a9c026d977
Springer114 <springerzack04@yahoo.com>
2021-04-07 16:55:25 -0700

    contact and footer components added

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -3,7 +3,16 @@ import React from 'react'
 const Contact = () => {
     return (
         <div>
-            
+            <div>
+                <a href="">Here's My Resume</a>
+            </div>
+            <h2>Contact</h2>
+            <form action="">
+                <input type="text" name="" id="" placeholder="Name"/>
+                <input type="email" name="" id="" placeholder="Email Address"/>
+                <input type="text" name="" id="" placeholder="Subject"/>
+                <textarea name="" id="" cols="30" rows="10" placeholder="Your message"></textarea>
+            </form>
         </div>
     )
 }
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            
+            <a href=""><img src="" alt="LinkedIn"/></a>
+            <a href=""><img src="" alt="GitHub"/></a>
+            <a href=""><img src="" alt="Treehouse"/></a>
+            <a href=""><img src="" alt="Email"/></a>
+            <footer>Zackary Springer ©2021</footer>
         </div>
     )
 }
