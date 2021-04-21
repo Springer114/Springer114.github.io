bfdac2bec3b1b284eb9a3a2e68ec7985d011bf5d
Springer114 <springerzack04@yahoo.com>
2021-04-21 13:15:52 -0700

    added form validations

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -2,7 +2,19 @@ import React, {useState} from 'react'
 import emailjs from 'emailjs-com'
 
 const Contact = () => {
-    const [contact, setContact] = useState();
+    const [contact, setContact] = useState({
+        name: "",
+        email: "",
+        subject: "",
+        message: ""
+    });
+    
+    const errorHandler = e => {
+        setContact({
+            ...contact,
+			[e.target.name] : e.target.value
+		});
+	}
 
     const serviceID = process.env.REACT_APP_SERVICE_ID;
     const templateID = process.env.REACT_APP_TEMPLATE_ID;
@@ -10,32 +22,35 @@ const Contact = () => {
 
     const submitHandler = e => {
         e.preventDefault();
-        
 
-        emailjs.sendForm('service_ab50ijs', 'template_qkynpzl', e.target, 'user_8gV2LbpIcmv6wgowbTHkI')
+        emailjs.sendForm(serviceID, templateID, e.target, userID)
             .then((response) => {
+                alert("Thank you for contacting me. I will try to get back to you as soon as I am able.")
                 console.log('Successful sendoff!', response.status, response.text);
             }, (error) => {
+                alert("Oops, message failed to send.")
                 console.log('Failed to send...', error);
         });
     };
-
 
     return (
         <div className="contact-container">
             <h2>Contact</h2>
             <form onSubmit={submitHandler}>
                 <div>
-                    <input type="text" name="name" placeholder="Name" onChange={e => setContact(e.target.value)} />
+                    <input type="text" name="name" placeholder="Name" onChange={errorHandler} />
+                    <p className="contact-error">{(contact.name.length > 1 || contact.name.length === 0) ? "" : "Name must be at least 2 characters."}</p>
                 </div>
                 <div>
-                    <input type="email" name="email" placeholder="Email Address" onChange={e => setContact(e.target.value)}/>
+                    <input type="email" name="email" placeholder="Email Address" onChange={errorHandler}/>
+                    <p className="contact-error"> {(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) ? "" : "You have entered an invalid email address."}</p>
                 </div>
                 <div>
-                    <input type="text" name="subject" placeholder="Subject" onChange={e => setContact(e.target.value)}/>
+                    <input type="text" name="subject" placeholder="Subject" onChange={errorHandler}/>
                 </div>
                 <div>
-                    <textarea name="message" cols="30" rows="10" placeholder="Your message" onChange={e => setContact(e.target.value)}></textarea>
+                    <textarea name="message" cols="30" rows="10" placeholder="Your message" onChange={errorHandler}></textarea>
+                    <p className="contact-error">{(contact.message.length > 1 || contact.message.length === 0) ? "" : "Message must be at least 2 characters."}</p>
                 </div>
                 <div>
                     <button>Send Message</button>
